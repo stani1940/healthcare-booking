4697d4e7c5a6a90ecf76fcc7af680e0bee892101
stani1940 <stanislav1940@abv.bg>
2026-09-19 15:12:19 +0300

    Add DoctorCard component and refactor Team component to use it for rendering doctor card profiles

---
+++ src/components/DoctorCard.jsx
@@ -0,0 +1,42 @@
+function DoctorCard({ doctor }) {
+  return (
+    <div className="col-md-4 col-sm-6">
+      <div className="team-thumb wow fadeInUp" data-wow-delay={doctor.delay}>
+        <img
+          src={doctor.image}
+          className="img-responsive"
+          alt=""
+        />
+
+        <div className="team-info">
+          <h3>{doctor.name}</h3>
+          <p>{doctor.specialty}</p>
+
+          <div className="team-contact-info">
+            <p>
+              <i className="fa fa-phone"></i> {doctor.phone}
+            </p>
+
+            <p>
+              <i className="fa fa-envelope-o"></i>{" "}
+              <a href="#">{doctor.email}</a>
+            </p>
+          </div>
+
+          <ul className="social-icon">
+            {doctor.socials.map((social, index) => (
+              <li key={index}>
+                <a
+                  href="#"
+                  className={`fa ${social.icon}`}
+                ></a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DoctorCard;
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,4 +1,45 @@
+import DoctorCard from "./DoctorCard";
 function team() {
+   const doctors = [
+    {
+      name: "Nate Baston",
+      specialty: "General Principal",
+      phone: "010-020-0120",
+      email: "general@company.com",
+      image: "/images/team-image1.jpg",
+      delay: "0.2s",
+      socials: [
+        { icon: "fa-linkedin-square" },
+        { icon: "fa-envelope-o" },
+      ],
+    },
+    {
+      name: "Jason Stewart",
+      specialty: "Pregnancy",
+      phone: "010-070-0170",
+      email: "pregnancy@company.com",
+      image: "/images/team-image2.jpg",
+      delay: "0.4s",
+      socials: [
+        { icon: "fa-facebook-square" },
+        { icon: "fa-envelope-o" },
+        { icon: "fa-flickr" },
+      ],
+    },
+    {
+      name: "Miasha Nakahara",
+      specialty: "Cardiology",
+      phone: "010-040-0140",
+      email: "cardio@company.com",
+      image: "/images/team-image3.jpg",
+      delay: "0.6s",
+      socials: [
+        { icon: "fa-twitter" },
+        { icon: "fa-envelope-o" },
+      ],
+    },
+  ];
+
     return (
          <section id="team" data-stellar-background-ratio="1">
       <div className="container">
@@ -14,113 +55,12 @@ function team() {
 
           <div className="clearfix"></div>
 
-          <div className="col-md-4 col-sm-6">
-            <div className="team-thumb wow fadeInUp" data-wow-delay="0.2s">
-              <img
-                src="/images/team-image1.jpg"
-                className="img-responsive"
-                alt=""
-              />
-
-              <div className="team-info">
-                <h3>Nate Baston</h3>
-                <p>General Principal</p>
-
-                <div className="team-contact-info">
-                  <p>
-                    <i className="fa fa-phone"></i> 010-020-0120
-                  </p>
-
-                  <p>
-                    <i className="fa fa-envelope-o"></i>{" "}
-                    <a href="#">general@company.com</a>
-                  </p>
-                </div>
-
-                <ul className="social-icon">
-                  <li>
-                    <a href="#" className="fa fa-linkedin-square"></a>
-                  </li>
-                  <li>
-                    <a href="#" className="fa fa-envelope-o"></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-sm-6">
-            <div className="team-thumb wow fadeInUp" data-wow-delay="0.4s">
-              <img
-                src="/images/team-image2.jpg"
-                className="img-responsive"
-                alt=""
-              />
-
-              <div className="team-info">
-                <h3>Jason Stewart</h3>
-                <p>Pregnancy</p>
-
-                <div className="team-contact-info">
-                  <p>
-                    <i className="fa fa-phone"></i> 010-070-0170
-                  </p>
-
-                  <p>
-                    <i className="fa fa-envelope-o"></i>{" "}
-                    <a href="#">pregnancy@company.com</a>
-                  </p>
-                </div>
-
-                <ul className="social-icon">
-                  <li>
-                    <a href="#" className="fa fa-facebook-square"></a>
-                  </li>
-                  <li>
-                    <a href="#" className="fa fa-envelope-o"></a>
-                  </li>
-                  <li>
-                    <a href="#" className="fa fa-flickr"></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-sm-6">
-            <div className="team-thumb wow fadeInUp" data-wow-delay="0.6s">
-              <img
-                src="/images/team-image3.jpg"
-                className="img-responsive"
-                alt=""
-              />
-
-              <div className="team-info">
-                <h3>Miasha Nakahara</h3>
-                <p>Cardiology</p>
-
-                <div className="team-contact-info">
-                  <p>
-                    <i className="fa fa-phone"></i> 010-040-0140
-                  </p>
-
-                  <p>
-                    <i className="fa fa-envelope-o"></i>{" "}
-                    <a href="#">cardio@company.com</a>
-                  </p>
-                </div>
-
-                <ul className="social-icon">
-                  <li>
-                    <a href="#" className="fa fa-twitter"></a>
-                  </li>
-                  <li>
-                    <a href="#" className="fa fa-envelope-o"></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+         {doctors.map((doctor) => (
+            <DoctorCard 
+            key={doctor.name} 
+            doctor={doctor} 
+            />
+          ))}
 
         </div>
       </div>
